e2c96227793a42323441a07dbde388e715b61e95
Sergey Lymar <lymars@valassis.com>
2019-01-04 14:02:13 +0200

    added vacancyadmin

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'city', 'specialty', 'date')
+
+    class Meta:
+        model = Vacancy
+
+
+
+
 admin.site.register(City)
-admin.site.register(Vacancy)
+admin.site.register(Vacancy, VacancyAdmin)
 admin.site.register(Specialty)
+admin.site.register(Website)
+admin.site.register(Url)
